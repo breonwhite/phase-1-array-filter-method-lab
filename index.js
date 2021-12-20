da4fa167e960a762ea12ffa21d7315c81ba5bba1
Breon White <bdwhite92@gmail.com>
2021-12-20 16:42:43 -0500

    commit tree 20818b2ba2c670bb21418bdc677665693ca9312a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(drivers, names) {
+    const matchFilter = drivers.filter((name) => {
+        if (name.toLowerCase() === names.toLowerCase()) {
+            return name
+        }
+    })
+    return matchFilter;
+}
+
+function fuzzyMatch(drivers, letters) {
+    const fuzzyFilter = drivers.filter((name) => {
+        if (name.startsWith(letters)) {
+            return name
+        }
+    })
+    return fuzzyFilter;
+}
+
+function matchName(drivers, properties) {
+    const nameFilter = drivers.filter(drivers => {
+        if (drivers.name === properties) {
+            return drivers
+        }
+    })
+    return nameFilter;
+}
